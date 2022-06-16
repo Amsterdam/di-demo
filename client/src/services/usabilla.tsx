58e80336a68e962e046cdfd1667d16fcbf0da714
janthijs <92784122+janthijs@users.noreply.github.com>
2022-06-16 15:44:01 +0200

    fix: allow for usabilla to switch languages

--- client/src/services/usabilla.tsx
+++ client/src/services/usabilla.tsx
@@ -1,9 +1,11 @@
 import '@services/lightning.js';
+import React, { useEffect } from 'react';
+import { Language, useCurrentLanguage } from './ContentProvider';
 import { isMobile } from './createIrmaSession';
 
 let usabilla_live: any;
 
-const initUsabilla = (): void => {
+const initUsabilla = (language: Language): void => {
     if (isMobile()) {
         usabilla_live = (window as any)['lightningjs'].require('usabilla_live', '//w.usabilla.com/7eb436c19ab1.js');
     } else {
@@ -23,4 +25,14 @@ export const startSurvey = (timeout = 2000): void => {
     }
 };
 
-initUsabilla();
+// initUsabilla();
+
+export const UsabillaProvider: React.FC = () => {
+    const language = useCurrentLanguage();
+
+    useEffect(() => {
+        initUsabilla(language);
+    }, [language]);
+
+    return <></>;
+};
